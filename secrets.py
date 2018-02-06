039cc2b41f774d4d428bdfecae5a0466b4c963df
Rob Dalka <rpdalka@umich.edu>
2018-02-06 16:19:00 -0500

    Added access token to secrets.py

--- secrets.py
+++ secrets.py
@@ -4,3 +4,6 @@
 
 api_key = 'ew9VPDjEgbYaTN8u9VkJyKqKq'
 api_secret = 'kqM2kfD1Llz72tmW3OHhDalfu5Z7oYUlFsGnbkrKWuH0ixYCKH'
+
+access_token = '1584699398-IDqDIkS9Xahp76Ov4F5uzzTnCe0oHLGLKPfSolM'
+access_token_secret = 'wpjomKdOqKzOO3d1ftkeTGgl4VfeL95jKvqHGvnCVifjI'
